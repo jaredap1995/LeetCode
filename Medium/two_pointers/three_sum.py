d1b65ff4899b2e7b13c6206ee80098e773ad33c8
jaredap1995 <jaredaperez1995@gmail.com>
2023-05-21 08:43:14 -0700

    .

--- Medium/two_pointers/three_sum.py
+++ Medium/two_pointers/three_sum.py
@@ -31,3 +31,16 @@ class Solution(object):
         return output
     
 print(Solution().threeSum(nums))
+
+
+"""This solution uses a set to check for suplicate triplets and a two pointer solution. In order to use two pointers the set must be sorted.
+Sort array first then create the set and the resulting output list. Iterate over the array and within the iteration create the two pointers.
+This approach is uniqye to the other twoSum problems because the two pointers are initialized within teh loop. This is what I intuitively
+tried to do, essentially creating three pointers (the third being the iterator). 
+
+
+We then go into the two pointer while loop, where j<k we check if the sum is equal to target 0. if it is we add all the numbers to the set and increment both j and k.
+If it is greater/less than we appropriately increment j or k and then return to the while loop and try again. If there is no succesful triplety combination
+from these three initialized numbers, eventually j will no longer be less tghan k and then we leave while loop and go back to for loop and initialize at a new point in array.
+
+We then turn the set into a list for our answer as teh set effectively prevented from any duplicates."""
