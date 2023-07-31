2aca2d9e8877fcff334e27075741aba3c88b80c1
jaredap1995 <jaredaperez1995@gmail.com>
2023-07-31 14:29:37 -0700

    .

--- JavaScript/Medium/backtracking/combination_sum2.js
+++ JavaScript/Medium/backtracking/combination_sum2.js
@@ -5,5 +5,24 @@
  */
 
 var combinationSum2 = function(candidates, target) {
-    
+    let result = []
+    candidates.sort(function(a,b) {return a-b})
+
+    var backtrack = (start, current_sum, current) => {
+        if (current_sum > target) {
+            return
+        }
+        if (current_sum == target) {
+            result.append([...current])
+            return
+        }
+        for (let i = start; i<candidates.length; i++){
+            if (i>start && candidates[i] == candiates [i-1]) continue 
+            current.push(candidates[i])
+            backtrack (start+1, current_sum+candidates[i], current)
+            current.pop()
+        }
+    }
+    backtrack(0,0,[])
+    return result
 }
