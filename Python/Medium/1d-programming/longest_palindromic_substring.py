baa463e86e8f3207a1a6c05d91c2af59bb248ef5
jaredap1995 <jaredaperez1995@gmail.com>
2023-08-15 14:07:31 -0700

    .

--- Python/Medium/1d-programming/longest_palindromic_substring.py
+++ Python/Medium/1d-programming/longest_palindromic_substring.py
@@ -1,10 +1,21 @@
-# class Solution:
-#     def longestPalindrome(s):
-#         dromes = {}
-#         i=0
-#         j=1
-#         while i<j:
-#             while i<j
-    
+class Solution:
+    def longestPalindrome(s):
+        longest = ''
+        dp_table=[[0]*len(s) for _ in range(len(s))]
+        for i in range(len(s)):
+            dp_table[i][i]=True
+            longest=s[i]
 
-# print(Solution.longestPalindrome(s))
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i+1, len(s)):
+                if s[i]==s[j]:
+                    if j-i == 1 or dp_table[i+1][j-1] is True:
+                        dp_table[i][j] = True
+                        if len(longest)<len(s[i:j+1]):
+                            longest=s[i:j+1]
+
+        return dp_table, longest
+
+    
+s="ababc"
+print(Solution.longestPalindrome(s))
