53d66c59e3e8da8152972e66b10098fb115a6671
jaredap1995 <jaredaperez1995@gmail.com>
2023-08-30 15:27:03 -0700

    .

--- SQL/join/placements.sql
+++ SQL/join/placements.sql
@@ -3,6 +3,6 @@ select name from (
     from friends f
     left join packages p on f.friend_id = p.id --friends salary
     left join students s on s.id = f.id
-    left join packages o on f.id = p.id ---students salary
+    left join packages o on f.id = o.id ---students salary
     where o.salary < p.salary --This comparison is valid because they are essentialy the salary of two different students, current student, and students friend
 ) cte order by friend_salary asc;
