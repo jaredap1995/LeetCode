8920981fbcc5da92fc7ac0543e4f50a59017ec8c
jaredap1995 <jaredaperez1995@gmail.com>
2023-09-19 17:05:36 -0700

    .

---
+++ Python/Easy/graphs/flood_fill.py
@@ -0,0 +1,24 @@
+class Solution:
+    def floodFill(image, sr, sc, color):
+        m = len(image)
+        n = len(image[0])
+        start = image[sr][sc]
+
+        def DFSsearch(i,j):
+            if i < 0 or j < 0 or i >=m or j >= n :
+                return
+            
+            image[i][j] = color
+
+            if image[i+1][j] == image[i][j]:
+                DFSsearch(i+1,j)
+            if image[i-1][j] == image[i][j]:
+                DFSsearch(i-1,j)
+            if image[i][j-1] == image[i][j]:
+                DFSsearch(i, j-1)
+            if image[i][j+1] == image[i][j]:
+                DFSsearch(i, j+1)
+
+        DFSsearch(sr, sc)
+
+        return image
