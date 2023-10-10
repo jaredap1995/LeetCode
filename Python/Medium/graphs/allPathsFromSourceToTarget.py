aa4d2012a5a04de39683b719a3d424d2055f6214
jaredap1995 <jaredaperez1995@gmail.com>
2023-10-10 12:46:12 -0700

    .

---
+++ Python/Medium/graphs/allPathsFromSourceToTarget.py
@@ -0,0 +1,30 @@
+import collections
+class Solution:
+    def allPathsSourceTarget(self, graph):
+        n=len(graph)
+        adj_list = collections.defaultdict(lambda: [])
+
+        for i in range(n):
+            adj_list[i]=(graph[i])
+
+        paths=[]
+        def DFS(cur, node):
+            cur=cur+[node]
+            if node == n-1:
+                paths.append(cur)
+
+            for neighbor in adj_list[node]:
+                DFS(cur, neighbor)
+            
+        
+        DFS([],0)
+        return paths
+    
+graph=[[4,3,1],[3,2,4],[3],[4],[]]
+
+print(Solution().allPathsSourceTarget(graph))
+
+
+
+
+        
