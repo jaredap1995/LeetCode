de148c6c4d2bce383d63a652591234d34a8d49fb
jaredap1995 <jaredaperez1995@gmail.com>
2023-11-04 17:23:57 -0700

    .

---
+++ JavaScript/Medium/graphs/pathWithMaxProb.js
@@ -0,0 +1,23 @@
+var maxProbability = function(n, edges, succProb, start_node, end_node) {
+    let probs = new Array(n).fill(0);
+    let graph = probs.reduce((accumulator, _, index) => accumulator.set(index, []), new Map());
+    edges.forEach(([node1, node2], index) => {
+        graph.get(node1).push([node2, succProb[index]])
+        graph.get(node2).push([node1, succProb[index]])
+    });
+
+    queue = [[start_node, 1]]
+    probs[start_node]=1
+
+    while(queue.length){
+        [curNode, curProb] = queue.shift();
+        for (const [neighbor, neighborProb] of graph.get(curNode)){
+            if ((curProb * neighborProb) > probs[neighbor]){
+                probs[neighbor] = curProb * neighborProb
+                queue.push([neighbor, prob[neighbor]])
+            }
+        }
+    }
+
+    return probs[end_node]
+}
