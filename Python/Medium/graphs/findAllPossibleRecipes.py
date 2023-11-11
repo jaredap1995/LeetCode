432b6648707c7f707c82382c983ebf35376b0ed7
jaredap1995 <jaredaperez1995@gmail.com>
2023-11-11 07:36:06 -0800

    .

--- Python/Medium/graphs/findAllPossibleRecipes.py
+++ Python/Medium/graphs/findAllPossibleRecipes.py
@@ -24,3 +24,29 @@ def Solution(recipes, ingredients, supplies):
             res.append(food)
 
     return res
+
+
+def Solution2_BFS(recipes, ingredients, supplies):
+    graph = collections.defaultdict(list)
+    # in_degree is counter for edges going in to each node
+    # If in_degree reaches 0 during BFS traversal, we can make the recipe
+    in_degree = collections.defaultdict(int)
+
+    # ingredient --> recipe in topological sort approach
+    for ingredient, recipe in zip(ingredients, recipes):
+        for ing in ingredient:
+            graph[ing].append(recipe)
+            in_degree[recipe]+=1
+
+    #BFS Traverse across all of supplies
+    queue = supplies[:]
+    res = []
+    while queue:
+        cur = queue.pop(0)
+        for neighbor in graph[cur]:
+            in_degree[neighbor] -= 1
+            if in_degree[neighbor] == 0:
+                res.append(neighbor)
+                queue.append(neighbor)
+
+    return res
