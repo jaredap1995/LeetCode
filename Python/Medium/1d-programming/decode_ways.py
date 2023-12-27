9ee86fde38d2889db9c9054ae59c6525279db2cc
jaredap1995 <jaredaperez1995@gmail.com>
2023-12-27 06:52:35 -0500

    .

--- Python/Medium/1d-programming/decode_ways.py
+++ Python/Medium/1d-programming/decode_ways.py
@@ -18,6 +18,30 @@ class Solution(object):
 s='AAJF'
 Solution().numDecodings(s)
 
+def solution(s):
+    decoder = {str(num) for num in range(1,27)}
+
+    @lru_cache #For memoization
+    def dfs(i):
+        #If we have reached the length of the string: we can decode it in one way ==> + 1
+        if i == len(s): return 1
+
+        # The value at that index is '0', so it is not a value to decode
+        if s[i] == '0': return 0
+
+        # Recursively call dfs function on the next charachter
+        res = dfs(i+1) 
+
+        # Up to the next charachter is less than length of string and the next two charachters are in decoder (ie '16')
+        if i + 1 < len(s) and s[i:i+2] in decoder:
+            res += dfs(i+2)
+
+        return res
+    
+    return dfs(0)
+    
+
+
 
 
 
