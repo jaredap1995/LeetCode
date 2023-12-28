24bdc4f6aff69dfe2f29acf7e0966db7643404af
jaredap1995 <jaredaperez1995@gmail.com>
2023-12-28 18:28:52 -0500

    .

---
+++ Python/Easy/Arrays_Hashing/imageSmoother.py
@@ -0,0 +1,23 @@
+import math
+class Solution:
+    def imageSmoother(self, img):
+        m = len(img)
+        n = len(img[0])
+
+        def gridHelper(row, col):
+            # set the total and count
+            total, count = 0,0
+
+            top = max(0, row-1)
+            bottom = min(m, row+2)
+            left = max(0, col - 1)
+            right = min(n, col+2)
+
+            for r in range(top, bottom):
+                for c in range(left, right):
+                    total += img[row][col]
+                    count += 1
+
+            return total // count
+        
+        return [[gridHelper(row, col) for row in m] for col in n]
