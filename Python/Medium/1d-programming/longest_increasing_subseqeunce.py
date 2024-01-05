40e4f360c6a127a58219eb4420f9af934e7f4db9
jaredap1995 <jaredaperez1995@gmail.com>
2024-01-04 20:25:50 -0800

    .

--- Python/Medium/1d-programming/longest_increasing_subseqeunce.py
+++ Python/Medium/1d-programming/longest_increasing_subseqeunce.py
@@ -18,3 +18,25 @@ for val in t:
         c+=1
 
 print(c)
+
+def solution2(nums):
+    if not nums: return 0
+
+    tails = []
+    for num in nums:
+        left = 0
+        right = len(nums)
+
+        while left < right:
+            mid = (left+right)//2
+            if mid < len(nums) and tails[mid] < num:
+                left = mid +1
+            else:
+                right = mid
+
+        if left >= len(tails):
+            tails.append(mid)
+        else:
+            tails[left] = num
+
+    return len(tails)
