ba5bb9c2b26be794feb25f0e4f5ad4de224c9697
Jared Perez <jaredaperez1995@gmail.com>
2024-02-02 18:20:01 -0800

    .

--- Python/Medium/1d-programming/partitionArrayforMaxSum.py
+++ Python/Medium/1d-programming/partitionArrayforMaxSum.py
@@ -5,9 +5,9 @@ def solution(arr, k):
     for i in range(1,n+1):
         maximum = 0
         max_sum = 0
-        for j in range(1,k):
+        for j in range(1,k+1):
             if i-j >=0:
-                maximum = max(maximum, dp[i-j])
+                maximum = max(maximum, arr[i-j])
                 max_sum = max(max_sum, dp[i-j] + maximum *j)
         dp[i] = max_sum
 
