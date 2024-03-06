397398df21ffaf7252c64f0d606a4ea35abaa078
Jared Perez <jaredaperez1995@gmail.com>
2024-03-05 19:15:02 -0800

    .

---
+++ Typescript/linkedLists/hasCycle.ts
@@ -0,0 +1,21 @@
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val? : number, next? : ListNode | null){
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null: next)
+    }
+}
+
+var hasCycle = (head: ListNode | null): boolean => {
+    let fast: ListNode | null = head
+    let slow: ListNode | null = head
+
+    while (fast && fast.next){
+        fast = fast.next.next
+        slow = slow.next
+        if (slow === fast) return false
+    }
+
+    return false
+}
