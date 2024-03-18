937bc828107fedbb2e4dcb54de2d61884c7ae075
Jared Perez <jaredaperez1995@gmail.com>
2024-03-18 06:54:34 -0700

    .

---
+++ Python/Medium/arrays_hashing/minNumberArraysPopBallons.py
@@ -0,0 +1,11 @@
+def solution(nums):
+    res = 0
+    maxa = float('-inf')
+    points = list(sorted(points), key = lambda x: x[1])
+
+    for i in range(len(points)):
+        if maxa < points[i][0]:
+            res +=1
+            maxa = points[i][1]
+
+    return res
