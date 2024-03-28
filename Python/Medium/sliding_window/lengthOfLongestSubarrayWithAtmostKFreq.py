da9867dead1e5df764fb494a9309c6a3dfa96784
Jared Perez <jaredaperez1995@gmail.com>
2024-03-28 07:24:05 -0700

    .

---
+++ Python/Medium/sliding_window/lengthOfLongestSubarrayWithAtmostKFreq.py
@@ -0,0 +1,14 @@
+def solution(nums, k):
+    hashmap = {}
+    length = 0
+    left = 0
+
+    for right in range(len(nums)):
+        hashmap[nums[right]] = hashmap.get(nums[right], 0) + 1
+        if hashmap[nums[right]] > k:
+            while nums[left] != nums[right]:
+                hashmap[nums[left]] -= 1
+                left += 1
+            hashmap[nums[left]] -= 1
+            left += 1
+        length = max(length, right - left + 1)
