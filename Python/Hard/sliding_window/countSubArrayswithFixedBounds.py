055528350465f8fb741a954e3649ca318a996a28
jaredap1995 <jaredaperez1995@gmail.com>
2024-03-31 12:21:29 -0700

    .

---
+++ Python/Hard/sliding_window/countSubArrayswithFixedBounds.py
@@ -0,0 +1,18 @@
+def solution(nums, minK, maxK):
+    minl = -1
+    maxl = -1
+    res = 0
+    left = 0
+
+    for right in range(len(nums)):
+        val = nums[right]
+        if val < minK or val > maxK:
+            minl = -1
+            maxl = -1
+            left = right + 1
+        else:
+            if val == minK:
+                minl = right
+            if val == maxK:
+                maxl = right
+            res += max(0, min(minl, maxl) - left + 1)
